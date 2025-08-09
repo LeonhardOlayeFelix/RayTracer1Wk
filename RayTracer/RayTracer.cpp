17e2169db761d57d32bcac2842b570ce5f8e83f0
Leonhard <leonhard.olaye-felix@student.manchester.ac.uk>
2025-08-09 20:45:25 +0100

    Implemented ray colour function and projection of rays onto viewport

--- RayTracer/RayTracer.cpp
+++ RayTracer/RayTracer.cpp
@@ -4,6 +4,7 @@
 #include "RayTracer.h"
 #include "vec3.h"
 #include "color.h"
+#include "ray.h"
 
 void MakeExampleImage() {
     // Image
@@ -19,15 +20,77 @@ void MakeExampleImage() {
         std::clog << "\r Scanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; i++) {
             auto pixel_color = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0);
-                write_color(std::cout, pixel_color);
+            write_color(std::cout, pixel_color);
         }
     }
     std::clog << "\rDone.                   \n";
+}
+
+color ray_color(const ray& r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+}
+
+void logInfo(point3 cameraCenter, double focal_length, vec3 viewport_u, vec3 viewport_v,
+    vec3 pixel_delta_u, vec3 pixel_delta_v,
+    int image_height, int image_width,
+    vec3 viewport_upper_left, vec3 pixel00_loc) {
+    std::clog << "Camera Center: " << cameraCenter << '\n';
+    std::clog << "focal_length: " << focal_length << '\n';
+    std::clog << "viewport_u: " << viewport_u << '\n';
+    std::clog << "viewport_v: " << viewport_v << '\n';
+    std::clog << "pixel_delta_u: " << pixel_delta_u << '\n';
+    std::clog << "pixel_delta_v: " << pixel_delta_v << '\n';
+    std::clog << "image_height: " << image_height << '\n';
+    std::clog << "image_width: " << image_width << '\n';
+    std::clog << "viewport_upper_left:" << viewport_upper_left << '\n';
+    std::clog << "pixel00_loc:" << pixel00_loc << '\n';
 }
 
 using namespace std;
 
 int main()
 {
-    MakeExampleImage();
+    auto aspect_ratio = 16.0 / 9.0;
+    int image_width = 256;
+
+    int image_height = int(image_width / aspect_ratio);
+    image_height = (image_height < 1) ? 1 : image_height;
+
+    //Camera
+    auto focal_length = 1.0;
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * (double(image_width) / image_height);
+    auto camera_center = point3(0, 0, 0);
+
+
+    //Calculating the vectors spanning the viewport
+    auto viewport_u = vec3(viewport_width, 0, 0);
+    auto viewport_v = vec3(0, -viewport_height, 0);
+
+    auto pixel_delta_u = viewport_u / image_width;
+    auto pixel_delta_v = viewport_v / image_height;
+
+    auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+
+    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_v + pixel_delta_u);
+
+    std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+
+    //logInfo(camera_center, focal_length, viewport_u, viewport_v, pixel_delta_u, pixel_delta_v, image_height, image_width, viewport_upper_left, pixel00_loc);
+
+    for (int j = 0; j < image_height; j++) {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+        for (int i = 0; i < image_width; i++) {
+            auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+            auto ray_direction = pixel_center - camera_center; 
+            ray r(camera_center, ray_direction);
+
+            color pixel_color = ray_color(r);
+            write_color(std::cout, pixel_color);
+        }
+    }
+
+    std::clog << "\rDone.                 \n";
 }
