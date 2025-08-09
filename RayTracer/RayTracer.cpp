003dad90cf8bdfab2f5d9b17a8536e03c4326113
Leonhard <leonhard.olaye-felix@student.manchester.ac.uk>
2025-08-09 13:55:33 +0100

    Created vec3 Class

--- RayTracer/RayTracer.cpp
+++ RayTracer/RayTracer.cpp
@@ -6,18 +6,19 @@
 void MakeExampleImage() {
     // Image
 
-    int image_width = 256;
-    int image_height = 256;
+    int image_width = 512;
+    int image_height = 512;
 
     // Render
 
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = 0; j < image_height; j++) {
+        std::clog << "\r Scanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; i++) {
             auto r = double(i) / (image_width - 1);
             auto g = double(j) / (image_height - 1);
-            auto b = 0.0;
+            auto b = 100;
 
             int ir = int(255.999 * r);
             int ig = int(255.999 * g);
@@ -26,6 +27,7 @@ void MakeExampleImage() {
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
+    std::clog << "\rDone.                   \n";
 }
 
 using namespace std;
---
+++ RayTracer/run_raytracer.bat
@@ -0,0 +1,3 @@
+@echo off
+cmake --build build
+build\RayTracer.exe > image.ppm
